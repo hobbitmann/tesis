194dc3b9d15d440dcc7f3210e97311b627a3d0b8
Gianni Rondalli Arenas <grondalli@gmail.com>
2017-03-04 22:16:18 -0300

    Change back to production environment

--- test1/Globals.swift
+++ test1/Globals.swift
@@ -6,7 +6,7 @@
 //  Copyright © 2017 gianni. All rights reserved.
 //
 
-//let baseURL = "http://pt202.dreamhosters.com"
-private let baseURL = "http://127.0.0.1"
+private let baseURL = "http://pt202.dreamhosters.com"
+//private let baseURL = "http://127.0.0.1"
 let apiUrl = "\(baseURL)/final/controllers/api"
 var loggedInUser = ""
